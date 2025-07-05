c8a2f27a3ce2fbf1da517029829a3329f71025f5
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-07-05 12:53:32 +0100

    fixed shortening API

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import Features from "./Components/Features";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Main from "./Components/Main";
 import Navigation from "./Components/Navigation";
-import ShortenedLink from "./Components/ShortenedLink";
-import ShortenLinkForm from "./Components/ShortenLinkForm";
 
 export default function App() {
   return (
     <>
       <Navigation />
       <Header />
-      <main className="px-[5%] md:px-[10%] py-8 flex flex-col gap-8">
-        <ShortenLinkForm />
-        <section className="flex flex-col gap-8">
-          <ShortenedLink />
-        </section>
-      </main>
+      <Main />
       <Features />
       <section className="bg-violet-950 p-16 px-[5%] md:px-[10%] text-white">
         <header className="text-center">
